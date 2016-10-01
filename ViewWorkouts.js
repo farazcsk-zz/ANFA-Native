ee8d0b39e2d23cdcf51399cd451d005558d8aa85
Faraz Khan <farazcsk@gmail.com>
2016-10-01 18:32:53 +0100

    Renders all workouts in view

--- ViewWorkouts.js
+++ ViewWorkouts.js
@@ -22,6 +22,7 @@ class ViewWorkouts extends Component {
     super(props, context);
   
     this.state = {
+    	workouts: []
     };
     this.getWorkouts = this.getWorkouts.bind(this);
   }
@@ -30,42 +31,43 @@ class ViewWorkouts extends Component {
     fetch("http://localhost:3000/api/Workouts?filter=%7B%22limit%22%3A%20%2210%22%7D&access_token=iTk6s6Boej92VgEFrKNnvg4rqD1uXjZmAUoNtHKgIqOwxi0LpnEToMK8SKYcjXuC")
     .then((response) => response.json())
     .then((responseData) => {
-      AlertIOS.alert(
-        "POST Response",
-        "Response Body -> " + JSON.stringify(responseData)
-      )
+      // AlertIOS.alert(
+      //   "POST Response",
+      //   "Response Body -> " + JSON.stringify(responseData)
+      // )
+      this.setState({workouts: responseData});
     })
     .done();
   }
   
   render() {
   	this.getWorkouts()
+	var workouts = this.state.workouts.map(function(workout) {
+		return (
+			<Text style={styles.welcome} >{workout.title}</Text>
+		); 
+	});
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-           MY WORKOUTS
-        </Text>
+      <View style={styles.workout}>
+        {workouts}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  workout: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F3F3F3',
+    borderRadius: 0,
+    borderWidth: 0.5,
+    borderColor: '#36333C'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#36333C',
-    marginBottom: 5,
   },
 });
 
